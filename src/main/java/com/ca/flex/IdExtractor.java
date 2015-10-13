d1a4609e003980e9a706db43aa2fe5f036c47df8
Greg Lawson <greglawson@gregs-mbp.man.co>
2015-10-13 11:42:40 -0400

    First Commit

---
+++ src/main/java/com/ca/flex/IdExtractor.java
@@ -0,0 +1,16 @@
+package com.ca.flex;
+
+public class IdExtractor {
+	
+	private String idRef;
+
+	public String getIdRef() {
+		return idRef;
+	}
+
+	public void setIdRef(String idRef) {
+		this.idRef = idRef;
+	}
+	
+
+}
